6bc966c90efe2fb61453c69a0e66b4b6458237b3
liupeng <liupeng_x00102@baosight.com>
2018-05-03 11:13:32 +0800

    'store注释'

--- src/components/testVuex.vue
+++ src/components/testVuex.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     这是测试vuex的基本使用示例
-    <p>price: {{price}} <button @click="incrementPrice(10)">price +10</button></p>
-    <p>count: {{count}} <button @click="incrementCount(50)">count +50</button></p>
+    <p>pid: {{pid}}</p>
+    <!--<p>price: {{price}} <button @click="incrementPrice(10)">price +10</button></p>-->
+    <!--<p>count: {{count}} <button @click="incrementCount(50)">count +50</button></p>-->
+    <p>price: {{checkoutPrice}} <button @click="incrementPrice(10)">price +10</button></p>
+    <p>count: {{checkoutCount}} <button @click="incrementCount(50)">count +50</button></p>
   </div>
 </template>
 
@@ -12,14 +15,30 @@
   export default {
     name: "test-vuex",
 
-    computed: mapGetters({
-      price: 'checkoutPrice',
-      count: 'checkoutCount'
-    }),
+    // computed: mapGetters({
+    //   price: 'checkoutPrice',
+    //   count: 'checkoutCount'
+    // }),
+    computed: {
+      ...mapState([
+        'pid'
+      ]),
+      ...mapGetters([
+        'checkoutPrice',
+        'checkoutCount'
+      ])
+    },
     methods: mapActions([
       'incrementPrice',
       'incrementCount'
-    ])
+    ]),
+
+    mounted(){
+      // mapGetters通过扩展运算符将store.state.checkoutPrice 映射this.checkoutPrice  这个this 很重要，这个映射直接映射到当前Vue的this对象上。
+      // 所以通过this都能将这些对象点出来，同理，mapState, mapActions, mapMutations都是一样的道理。牢记~~~
+      console.log(typeof checkoutPrice); // undefined
+      console.log(typeof this.checkoutPrice);  // number
+    }
   }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -5,14 +5,15 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
+    pid: "123",
     price: 10,
     count: 100
   },
-  getters: {
+  getters: {  // 用于vuex中mapGetter,获取一些处理后的响应值
     checkoutPrice: state => state.price,
     checkoutCount: state => state.count,
   },
-  actions: {
+  actions: {  // 用于vuex中mapActions,获取一些业务上的方法(commit调用mutations中的操作)
     incrementPrice ({state, commit}, priceIncrement) {
       commit('increment', {priceIncrement: priceIncrement})
     },
@@ -20,7 +21,7 @@ const store = new Vuex.Store({
       commit('increment', {countIncrement: countIncrement})
     }
   },
-  mutations: {
+  mutations: {  // 唯一更新state中数据的地方，理解为抽象的actions
     increment (state, {priceIncrement, countIncrement}) {
       state.price += priceIncrement || 0;
       state.count += countIncrement || 0;
@@ -28,4 +29,4 @@ const store = new Vuex.Store({
   }
 });
 
-export default store
+export default store  // 导出Vuex.Store,供main.js中vue实例化时加载
