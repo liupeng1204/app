ede0284307c5ae2470af3091fc9e88cd48b7f2d4
liupeng <liupeng_x00102@baosight.com>
2018-05-23 21:35:58 +0800

    '路由过渡动效transition'

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,16 @@
   <div id="app">
     <img src="./assets/logo.png">
 
+    <!--过渡动效:CSS 动画-->
+    <transition name="bounce">
+      <router-view/>
+    </transition>
+
     <!--路由命名视图-->
-    <router-view/>
     <router-view name="routerViewName"/>
     <router-view name="routerViewName2"/>
+
+
   </div>
 </template>
 
@@ -16,12 +22,32 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .bounce-enter-active {
+    animation: bounce-in .5s;
+  }
+  .bounce-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+
+
 </style>
